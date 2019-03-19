1fcb9db1e3621685c655ca4e891a3b9f55e5fc39
stevenamedee <44212481+stevenamedee@users.noreply.github.com>
2019-03-19 11:03:46 -0500

    Add files via upload

--- js_files/pw_strength.js
+++ js_files/pw_strength.js
@@ -1,7 +1,73 @@
+/*var complexity;
+var len;
+function pwCheck() {
+	var val = document.getElementById("pwtxt").value;
+	
+	len = val.length >= 10;	//is the pw at least 10 characters?
+	var dig = /\d/.test(val);	//contains digit?
+	var low = /[a-z]/.test(val);	//contains lowercase letter?
+	var up = /[A-Z]/.test(val);	//contains uppercase letter?
+	var spec = /[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/.test(val); //contains special character?
+	complexity = dig + low + up + spec; //OWASP recommends at least 3 of these be met
+	//console.log("Compelxity: " + complexity);
+	var requirementsMet = len && complexity >= 3;
+	if (requirementsMet) {
+		//console.log("Password meets requirements");
+		var s = "Requirements Met";
+	} else {
+		var s = "Requirements Not Met"; 
+	}
+	document.getElementById("req").innerHTML = s;
+	
+	//change the colors of the requirements if they are met:
+	document.getElementById("pwLen").style.color = len ? "green" : "red";
+	document.getElementById("pwLow").style.color = low ? "green" : "red";
+	document.getElementById("pwUp").style.color = up ? "green" : "red";
+	document.getElementById("pwNum").style.color = dig ? "green" : "red";
+	document.getElementById("pwSpe").style.color = spec ? "green" : "red";
+	
+}
+function checkInfo() {
+	var msg = "";
+	var pw = document.getElementById("pwtxt").value;
+	var conf = document.getElementById("confirmPw").value;
+	console.log(pw);
+	console.log(conf);
+	console.log(len);
+	if (pw == "") {
+		msg = "Please enter a password";
+	} else if (len == false) {
+		msg = "Password must be at least 10 characters";
+	} else if (complexity < 3) {
+		msg = "Password doesn't meet requirements";
+	} else if (pw != conf){
+		msg = "Passwords don't match";
+	}
+	
+	if (msg != ""){
+		alert(msg);
+		return false;
+	} else {
+		document.getElementById("req").innerHTML = "Password successfully entered.";
+		return true;
+	}
+	
+}
+
+function togglePW(id) {
+	var pw = document.getElementById(id);
+	if (pw.type === "password") {
+		pw.type = "text";
+	} else {
+		pw.type = "password";
+	}
+}
+*/
+
+
 var complexity;
-var len = false;
+var len;
 var meterLen = 0;
-var noMatch = false;
 var scoreValue = {0: 0, 1: 25, 2: 50, 3: 75, 4: 100};
 function pwCheck() {
 	var val = document.getElementById("pwtxt").value;
@@ -12,6 +78,7 @@ function pwCheck() {
 	var up = /[A-Z]/.test(val);	//contains uppercase letter?
 	var spec = /[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/.test(val); //contains special character?
 	complexity = dig + low + up + spec; //OWASP recommends at least 3 of these be met
+	//console.log("Compelxity: " + complexity);
 	var requirementsMet = len && complexity >= 3;
 	if (requirementsMet) {
 		//console.log("Password meets requirements");
@@ -33,40 +100,33 @@ function pwCheck() {
 	
 	return scoreValue[result.score];
 }
-function checkPasswordMatch() {
-	var pw = document.getElementById("pwtxt");
-	var conf = document.getElementById("confirmPw");
-	if(pw.value != conf.value) {
-		conf.setCustomValidity("Passwords must match");
-		noMatch = true;
-	} else {
-		conf.setCustomValidity("");
-		console.log("Password successfully entered");
+function checkInfo() {
+	var msg = "";
+	var pw = document.getElementById("pwtxt").value;
+	var conf = document.getElementById("confirmPw").value;
+	console.log(pw);
+	console.log(conf);
+	console.log(len);
+	if (pw == "") {
+		msg = "Please enter a password";
+	} else if (len == false) {
+		msg = "Password must be at least 10 characters";
+	} else if (complexity < 3) {
+		msg = "Password doesn't meet requirements";
+	} else if (pw != conf){
+		msg = "Passwords don't match";
+		//gives an error that the PWs don't match even though they are
 	}
-
-}
-
-function confirmPass(){
-	console.log("In confirmPass");
-	if (!noMatch){
-		console.log("Returning");
-		return;
+	
+	if (msg != ""){
+		alert(msg);
+		return false;
 	} else {
-		var pw = document.getElementById("pwtxt");
-		var conf = document.getElementById("confirmPw");
-		console.log(pw.value);
-		console.log(conf.value);
-		if(pw.value != conf.value) {
-			console.log("no match");
-			conf.setCustomValidity("Passwords Don't Match");
-			conf.reportValidity();
-		} else {
-			conf.setCustomValidity("");
-			console.log("Password successfully entered");
-		}
+		document.getElementById("req").innerHTML = "Password successfully entered.";
+		return true;
 	}
+	
 }
-
 function togglePW(id) {
 	var pw = document.getElementById(id);
 	if (pw.type === "password") {
@@ -76,8 +136,25 @@ function togglePW(id) {
 	}
 }
 
-
-
+function changeMeter(bar, start, stop, barTxt){
+	var x = start < stop ? 1 : -1;
+	console.log("In change meter");
+	var width = start;
+	console.log("Starting width: " + start);
+	console.log("Stopping width: " + stop);
+	var id = setInterval(frame, 12);
+	function frame() {
+		if (width === stop) {
+			clearInterval(id);
+		} else {
+			width += x;
+			//console.log(width);
+			if ((width > stop && x > 0) || (width < stop && x < 0)) width = stop; //to ensure the percentage stays within bounds
+			bar.style.width = width + '%'; 
+			if (barTxt) barTxt.innerHTML = ~~width + '%';
+		}
+	}
+}
 $(function() {
     $("#pwtxt").on("input", function () {
         var x = pwCheck();
